7ceb8f2307670bfaf50c525b0d37650cabfd96ed
DewerDarkGamer <104209498+DewerDarkGamer@users.noreply.github.com>
2025-08-20 17:15:39 +0700

    Update MainForm.Designer.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,12 +1,14 @@
-// MainForm.Designer.cs
 namespace BarcodeRename
 {
     partial class MainForm
     {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -20,45 +22,18 @@ namespace BarcodeRename
 
         #region Windows Form Designer generated code
 
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
-            this.btnLoadImage = new System.Windows.Forms.Button();
-            this.txtResult = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // btnLoadImage
-            // 
-            this.btnLoadImage.Location = new System.Drawing.Point(12, 12);
-            this.btnLoadImage.Name = "btnLoadImage";
-            this.btnLoadImage.Size = new System.Drawing.Size(120, 40);
-            this.btnLoadImage.TabIndex = 0;
-            this.btnLoadImage.Text = "Load Image";
-            this.btnLoadImage.UseVisualStyleBackColor = true;
-            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
-            // 
-            // txtResult
-            // 
-            this.txtResult.Location = new System.Drawing.Point(12, 70);
-            this.txtResult.Multiline = true;
-            this.txtResult.Name = "txtResult";
-            this.txtResult.Size = new System.Drawing.Size(360, 200);
-            this.txtResult.TabIndex = 1;
-            // 
-            // MainForm
-            // 
-            this.ClientSize = new System.Drawing.Size(400, 300);
-            this.Controls.Add(this.txtResult);
-            this.Controls.Add(this.btnLoadImage);
-            this.Name = "MainForm";
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 400);
             this.Text = "Barcode Rename";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnLoadImage;
-        private System.Windows.Forms.TextBox txtResult;
     }
 }
